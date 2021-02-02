cbf158009c46966454cbebdd2052ab92df823063
sdaskaliesku <manson.serg@gmail.com>
2021-02-02 18:25:23 +0200

    Fix comment

--- run.bat
+++ run.bat
@@ -1,3 +1,2 @@
+:: Remove `clean package` if you've already built the code
 mvnw.cmd clean package exec:java -Dexec.args="-m patch -in \"src/test/resources/original seventysixmenu swf.xml\" -config src\test\resources\config.json -out src\test\resources\patched.xml
-:: Comment out first line and uncomment next one, if you already run this program
-:: mvnw.cmd exec:java -Dexec.args="-i src\test\resources\config.json"
